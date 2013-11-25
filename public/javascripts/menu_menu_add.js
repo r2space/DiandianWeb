d420f4220e2e4fb52eab36a99d75934956705a0f
sara <fyx1014@hotmail.com>
2013-11-25 10:44:36 +0800

    change for menu

--- public/javascripts/menu_menu_add.js
+++ public/javascripts/menu_menu_add.js
@@ -6,119 +6,119 @@
 $(function () {
   'use strict';
 
-  var menuId = $("#menuId").val();
-  events(menuId);
-  render(menuId);
+  var pages = 0;
+  var pageSizeMap = {
+    1: {row: 1, col: 1},
+    2: {row: 2, col: 2},
+    3: {row: 3, col: 3}
+  };
 
-});
+  var onGrid = 1;
+  var grids = [];
+  var menu = {};
+  menu.id = $("#menuId").val();
 
-function events(menuId) {
+  events(menu);
+  render(menu.id);
 
-  // 大格页
-  $('#addLargeMenuScreen').on("click", function(){
-    insertLandscapeScreenAfter(1);
-  });
-  // 中格页
-  $('#addMediumMenuScreen').on("click", function(){
-    insertLandscapeScreenAfter(2);
-  });
-  // 小格页
-  $('#addSmallMenuScreen').on("click", function(){
-    insertLandscapeScreenAfter(3);
-  });
-  // 选择确定的临时动作 TODO
-  $("#saveItems").on("click", function(){
-    $("#itemModal").modal("hide");
-  });
+  function events(menu) {
 
-  $("#saveMenu").on("click", function(event){
+    // 大格页
+    $('#addLargeMenuScreen').on("click", function(){
+      insertLandscapeScreenAfter(1);
+    });
+    // 中格页
+    $('#addMediumMenuScreen').on("click", function(){
+      insertLandscapeScreenAfter(2);
+    });
+    // 小格页
+    $('#addSmallMenuScreen').on("click", function(){
+      insertLandscapeScreenAfter(3);
+    });
+    // 选择确定的临时动作 TODO
+    $("#saveItems").on("click", function(){
+      $("#itemModal").modal("hide");
+    });
 
-    var menu = {
-      name: $("#menuName").val()
-      , comment: $("#menuComment").val()
-      , status: $("#inputStatus").attr("value")
-    };
+    $("#saveMenu").on("click", function(event){
 
-    if (!check_menu(menu)) {
+        menu.name: $("#menuName").val()
+        , comment: $("#menuComment").val()
+        , status: $("#inputStatus").attr("value")
+      };
 
-      if (menuId) {
-
-        menu.id = menuId;
-        smart.dopost("/menu/update.json", menu, function(err, result) {
-          if (err) {
-            smart.error(err,i18n["js.common.update.error"],false);
-          } else {
-            window.location = "/menu/menu/list";
-          }
-        });
-      } else {
-        smart.dopost("/menu/add.json", menu, function(err, result) {
-          if (err) {
-            smart.error(err,i18n["js.common.add.error"],false);
-          } else {
-            window.location = "/menu/menu/list";
-          }
-        });
-      }
-    }
-  });
-}
-
-function insertLandscapeScreenAfter(screenNum){
-
-  var main = $("#layoutLandscapeMain");
-  main.css( "width", "+=549" );
-
-  if (screenNum === 1) {
-    main.append($("#largePageHidden").html());
-  } else if(screenNum === 2){
-    main.append($("#mediumPageHidden").html());
-  } else if (screenNum === 3){
-    main.append($("#smallPageHidden").html());
-  }
-
-  // 菜品选择
-  $.each($(".itemDiv"), function(idx, item) {
-    item.onclick = function(event){
-      // 记录当前DIV TODO
-      $("#itemModal").modal("show");
-    };
-  });
-
-  var space = $("#layoutLandscape");
-
-  space.animate({
-    scrollLeft: main.width()-549
-  }, 400);
-}
-
-function render(menuId) {
-
-  if (menuId) {
-
-    smart.doget("/menu/findOne.json?id=" + menuId , function(err, result) {
-      if (err) {
-        smart.error(err,i18n["js.common.search.error"],false);
+      if (menu.name == "") {
+        Alertify.log.error(i18n["js.public.check.menu.name"]);
       } else {
 
-        $("#menuName").val(result.name);
-        $("#menuComment").val(result.comment);
-        new ButtonGroup("inputStatus", result.status).init();
+        if (menu.id) {
+
+          menu.id = menuId;
+          smart.dopost("/menu/update.json", menu, function(err, result) {
+            if (err) {
+              smart.error(err,i18n["js.common.update.error"],false);
+            } else {
+              window.location = "/menu/menu/list";
+            }
+          });
+        } else {
+          smart.dopost("/menu/add.json", menu, function(err, result) {
+            if (err) {
+              smart.error(err,i18n["js.common.add.error"],false);
+            } else {
+              window.location = "/menu/menu/list";
+            }
+          });
+        }
       }
     });
-  } else {
-    new ButtonGroup("inputStatus", 0).init();
-  }
-}
-
-function check_menu(menu) {
-
-  var flag = 0;
-  if (menu.name == "") {
-    Alertify.log.error(i18n["js.public.check.menu.name"]);
-    flag = 1;
   }
 
-  return flag;
-}
+  function insertLandscapeScreenAfter(screenNum){
 
+    var main = $("#layoutLandscapeMain");
+    main.css( "width", "+=549" );
+
+    if (screenNum === 1) {
+      main.append($("#largePageHidden").html());
+    } else if(screenNum === 2){
+      main.append($("#mediumPageHidden").html());
+    } else if (screenNum === 3){
+      main.append($("#smallPageHidden").html());
+    }
+
+    // 菜品选择
+    $.each($(".itemDiv"), function(idx, item) {
+      item.onclick = function(event){
+        // 记录当前DIV TODO
+        $("#itemModal").modal("show");
+      };
+    });
+
+    var space = $("#layoutLandscape");
+
+    space.animate({
+      scrollLeft: main.width()-549
+    }, 400);
+  }
+
+  function render(menuId) {
+
+    if (menuId) {
+
+      smart.doget("/menu/findOne.json?id=" + menuId , function(err, result) {
+        if (err) {
+          smart.error(err,i18n["js.common.search.error"],false);
+        } else {
+
+          $("#menuName").val(result.name);
+          $("#menuComment").val(result.comment);
+          new ButtonGroup("inputStatus", result.status).init();
+        }
+      });
+    } else {
+      new ButtonGroup("inputStatus", 0).init();
+    }
+  }
+
+});
