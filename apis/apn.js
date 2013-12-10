76834d9f387a1014554cdaa40cd0e357914c220b
Li Hao <7923495@qq.com>
2013-12-10 14:48:16 +0800

    发布

--- apis/apn.js
+++ apis/apn.js
@@ -9,9 +9,9 @@ exports.testApn = function(req, res){
 }
 function sendApn(){
   var options = {
-    cert: './pem/diandian.pem',                 /* Certificate file path */
-    key:  './pem/diandian_key.pem',                  /* Key file path */
-    gateway: 'gateway.push.apple.com',/* gateway address gateway.push.apple.com, port 2195*/
+    cert: './pem/pro.pem',                 /* Certificate file path */
+    key:  './pem/pro_key.pem',                  /* Key file path */
+    gateway: 'gateway.push.apple.com',/* gateway address  gateway.sandbox.push.apple.com  gateway.push.apple.com, port 2195*/
     port: 2195,                       /* gateway port */
     batchFeedback: true,
     errorCallback: errorHappened     /* Callback when error occurs function(err,notification) */
@@ -28,7 +28,7 @@ function sendApn(){
 
   var apnsConnection = new apns.Connection(options);
 
-  var myDevice = new apns.Device("b762a373 6717c4e2 1cd1ebc8 0b695cd7 8cd544c0 f49ad0fa 80eb5dec f9845687");
+  var myDevice = new apns.Device("84df75fc 6aa230a3 d66e8d6c d147cd4a 29be2b3c 2124a601 388d383c b09552e8");
   var note = new apns.Notification();
   note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
   note.badge = 1;
