ac0b71737fc169725d2fd2493149cf2393e9a941
Li Hao <7923495@qq.com>
2013-12-16 10:43:28 +0800

    发布发布

--- apis/file.js
+++ apis/file.js
@@ -11,6 +11,16 @@ var context              = smart.framework.context
   , response             = smart.framework.response
   , file                 = require("../controllers/ctrl_file");
 
+function createErrorSchema(errCode, errMessage, errorDetail) {
+  return {
+    apiVersion: "1.0"
+    , error: {
+      code: errCode
+      , message: errMessage
+      , errors: errorDetail
+    }
+  };
+}
 /**
  * 获取店铺摄影
  * @param req 请求对象
@@ -22,10 +32,22 @@ exports.getImage = function(req, res) {
   var handler = new context().bind(req, res);
 
   log.operation("begin: get image.", handler.uid);
-  file.getImage(handler, function(err, result) {
+  file.getImage(handler, function(error, data) {
 
-    log.operation("finish: get image.", handler.uid);
 
-    response.sendFile(res, err, result);
+    // 返回错误信息
+    if (error) {amtlib.framework
+      return res.send(error.code, createErrorSchema(error.code, error.message));
+    }
+    res.setHeader("Content-Type", data.fileInfo.contentType);
+    res.setHeader("Content-Length", data.fileInfo.length);
+
+    // 返回JSON数据
+    var expires = new Date();
+    expires.setTime(expires.getTime() + 60 * 60 * 24 * 365 * 1000);
+    res.setHeader("Expires", expires.toUTCString());
+    res.setHeader("Cache-Control", "public, max-age=" + 60 * 60 * 24 * 365);
+    res.setHeader("Last-Modified", data.fileInfo.updateAt);
+    return res.send(data.fileData);
   });
 };
